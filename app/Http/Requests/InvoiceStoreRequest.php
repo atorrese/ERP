d353f01b971288a1ca49bd7cc8e3fcaf74a85262
Ángel Alberto Torres Espinoza <atorrese@unemi.edu.ec>
2021-02-24 20:04:11 -0500

    Creating rules of validation  for table invoices that used in function store on Controller

---
+++ app/Http/Requests/InvoiceStoreRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class InvoiceStoreRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'client_id' => 'required',
+            'user_id' => 'required'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'client_id.required' => 'El campo es obligatorio.',
+            'user_id.required' => 'El campo es obligatorio.'
+        ];
+    }
+    public function authorize()
+    {
+        return true;
+    }
+}
